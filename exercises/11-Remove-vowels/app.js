e5beba1537078c69f5c927565c1d002c6f3f6de5
silvinaas <silvinaas4@gmail.com>
2021-09-05 20:01:54 +0000

    me falta el ej 11

--- exercises/11-Remove-vowels/app.js
+++ exercises/11-Remove-vowels/app.js
@@ -5,9 +5,11 @@ const rapid = (myString) => {
         if(['a','e','i','o','u'].includes(myString[position]) == false)
             consonants.push(myString[position])
     }
+
     return consonants.join('');
 }
 
 // From this line up Do not change code below
 let str = "John";
+str = str.toUpperCase();
 console.log(rapid(str));
